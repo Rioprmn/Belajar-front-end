f40c3c845e7b13b1bc1dfc49cf316d7d89fff2f8
Rio Permana <githubrio@gmail.com>
2026-02-12 15:01:32 +0700

    js dasar

---
+++ JS dasar/latihan15.js
@@ -0,0 +1,12 @@
+//object literal
+var mahasiswa = {
+    mahasiswa : "Rizki Adam Kurniawan",
+    umur : 20,
+    ips : [3.00, 3.10, 3.20, 3.30],
+    alamat : {
+        jalan : "Jl. Mawar",
+        kota : "Bandung",
+        provinsi : "Jawa Barat"
+    }
+};
+
